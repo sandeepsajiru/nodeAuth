8f6479482a72224d1b59436ad406b78a42cffff9
Sandeep <sandeep.sajiru@gmail.com>
2015-12-10 14:35:27 +0530

    Updating Endpoints and Adding Mailer Mock TODO

--- test/mailer-mock.js
+++ test/mailer-mock.js
@@ -2,7 +2,7 @@ var MailerMock = function () {
 };
 
 MailerMock.prototype.sendPasswordResetHash = function (email, passwordResetHash) {
-
+    // TODO:  Write Code to Send Email
 };
 
 module.exports = MailerMock;
